bcd54d2783f4c902b99b86c6abd3c86ed260076a
Milandeep <kmilandeep015@gmail.com>
2025-04-18 19:58:55 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React from "react";
-import {BrowserRouter , Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './Home.jsx';
 import About from "./About.jsx";
 import Project from "./Project.jsx";
 import Experience from "./Experience.jsx";
 import Contact from "./Contact.jsx";
+
 function App() {
   return (
-    <>
-      <BrowserRouter>
-       <Routes>
-     
-
-         <Route path ="/" element={<Home />} />
-         <Route path ="/about" element={<About />}/> 
-         <Route path ="/project" element={<Project />}/>
-         <Route path ="/experience" element={<Experience />}/> 
-         <Route path ="/contact" element={<Contact />}/> 
-       </Routes>
-     </BrowserRouter>
-    </>
+    <BrowserRouter basename="/portfolio">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/project" element={<Project />} />
+        <Route path="/experience" element={<Experience />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
